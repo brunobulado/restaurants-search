c267e59f780f4cf1003d31c2cbb3bd5742d64739
brunobulado <brunos.bulado@gmail.com>
2025-09-25 00:22:17 -0300

    indo dormir

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import ReactStars from 'react-rating-stars-component';
 
-import { Restaurant } from './styles';
+import restaurante from '../../assets/restaurante-fake.png';
 
-const RestaurantCard = () => <Restaurant />;
+import { Restaurant, RestaurantInfo, RestaurantPhoto, Title, Address } from './styles';
+
+const RestaurantCard = () => (
+  <Restaurant>
+    <RestaurantInfo>
+      <Title>Restaurant Name</Title>
+      <ReactStars count={5} isHalf value={4} edit={false} activeColor="#E7711C" />
+      <Address>Rua do Teste, 1091</Address>
+    </RestaurantInfo>
+    <RestaurantPhoto src={restaurante} alt="Foto do Restaurante" width="88" height="88" />
+  </Restaurant>
+);
 
 export default RestaurantCard;
