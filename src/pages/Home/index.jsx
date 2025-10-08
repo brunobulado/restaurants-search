d39dc5d7312fcb6ac41cc9108b3fdd3439e1ef4e
brunobulado <brunos.bulado@gmail.com>
2025-10-07 23:55:52 -0300

    07-10

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,10 +6,11 @@ import MaterialIcon from '@material/react-material-icon';
 import logo from '../../assets/logo.svg';
 import { Container, Carousel, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
 import restaurante from '../../assets/restaurante-fake.png';
-import { Card, RestaurantCard } from '../../components';
+import { Card, RestaurantCard, Modal } from '../../components';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
+  const [modalOpened, setModalOpened] = useState(false);
 
   const settings = {
     dots: false,
@@ -40,10 +41,14 @@ const Home = () => {
             <Card photo={restaurante} title="Harumaki" />
             <Card photo={restaurante} title="Churrasco Gaúcho" />
           </Carousel>
+          {/* <button type="button" onClick={() => setModalOpened(true)}>
+            Abrir Modal
+          </button> */}
         </Search>
         <RestaurantCard />
       </Container>
       <Map />
+      <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} />
     </Wrapper>
   );
 };
